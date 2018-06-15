4a950a9289985aca4ba620608363c498d0c0c1e9
YanxiSir <yanxisir@gmail.com>
2018-06-15 18:47:34 +0800

    task

--- src/main/java/com/yanxisir/fetealarm/schedule/BirthdaySchedule.java
+++ src/main/java/com/yanxisir/fetealarm/schedule/BirthdaySchedule.java
@@ -13,7 +13,6 @@ import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
